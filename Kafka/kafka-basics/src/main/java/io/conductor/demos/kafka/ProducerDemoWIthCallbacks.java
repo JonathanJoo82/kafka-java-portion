728779afa2a5a389f2d73395c9b3805287ab7c42
JonathanJoo82 <jonathanjoo@gmail.com>
2024-07-09 19:37:53 -0500

    difggff

--- Kafka/kafka-basics/src/main/java/io/conductor/demos/kafka/ProducerDemoWIthCallbacks.java
+++ Kafka/kafka-basics/src/main/java/io/conductor/demos/kafka/ProducerDemoWIthCallbacks.java
@@ -1,17 +1,18 @@
 package io.conductor.demos.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.datatransfer.StringSelection;
 import java.util.Properties;
 
-public class ProducerDemo {
+public class ProducerDemoWIthCallbacks {
 
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWIthCallbacks.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("hello world");
@@ -44,7 +45,18 @@ public class ProducerDemo {
         ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
 
         //send data
-        producer.send(producerRecord);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                //executes every time a record is sent or an exception is made.
+                if (exception == null){
+                    //was sent successfully
+                    log.info("message sent by the producer" + metadata.topic() + metadata.partition() + metadata.offset());
+                } else {
+                    log.error("exception");
+                }
+            }
+        });
 
         //flush(send all data and block until done) and close the producer
         producer.flush();
